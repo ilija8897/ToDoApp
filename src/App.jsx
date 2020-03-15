eaafa6bceb39584d9d58950b825aa30c505f51a5
Ilia <hello@ilija.ru>
2020-03-15 11:35:21 +0300

    Initial commit from create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./App.module.sass";
+
+export const App = () => {
+  return (
+    <div className={style.app}>
+      <header className={style.appHeader}>
+        <h2>TO_DO_APP</h2>
+      </header>
+      <main className={style.mainSection}>
+      </main>
+    </div>
+  );
+};
